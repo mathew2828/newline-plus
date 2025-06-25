95006dda29a2476e0919c79905e82a0f445e7243
mathew <90034902+mathewwww@users.noreply.github.com>
2025-06-25 21:27:03 +0800

    Initial commit

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import {Grid2, Button} from '@mui/material'
+
+function Home() {
+    return (
+      <Grid2>
+          <Button variant='contained'> Hello Button </Button>
+        
+      </Grid2>
+    );
+  }
+  
+  export default Home;
+  
